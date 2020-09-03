0e81962ca1940b5dcd0e3dec12105ebdcca0af32
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-09-03 13:02:51 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
+    (1, 'the_matrix', 1, 1),
+    (2, 'the_ring', 2, 2);
+
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
