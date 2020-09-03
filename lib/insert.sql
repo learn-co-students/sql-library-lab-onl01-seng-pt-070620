700c8bde8689e53590cc8f5fe1c817371a6c1f77
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-09-03 16:24:13 -0700

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,4 +10,38 @@ INSERT INTO books (id, title, year, series_id) VALUES
     (5, 'enlightenment now', 2010, 5),
     (6, 'the new jim crow', 2010, 6);
 
-    
+INSERT INTO characters (id, name, motto, species, author_id) VALUES 
+    (1, 'neo', 'it is done', 'savior', 1),
+    (2, 'morphes', 'i believe', 'mentor', 2),
+    (3, 'trinnity', 'dodge this', 'lover', 3),
+    (4, 'dexter', 'you deserve it', 'killer', 4),
+    (5, 'john', 'you are dead', 'low key', 5),
+    (6, 'snake', 'no words', 'hero', 6),
+    (7, 'leon', 'ada', 'solo', 7),
+    (8, 'chris', 'jill!', 'leader', 8);
+
+INSERT INTO subgenres (id, name) VALUES
+    (1, 'fantasy'),
+    (2, 'sci-fi');
+
+INSERT INTO authors (id, name) VALUES
+    (1, 'john green'),
+    (2, 'karl marx');
+
+INSERT INTO character_books (id, book_id, character_id) VALUES
+    (1, 1, 1), 
+    (2, 1, 2), 
+    (3, 2, 2), 
+    (4, 3, 2), 
+    (5, 1, 3), 
+    (6, 2, 3), 
+    (7, 3, 3), 
+    (8, 1, 4),
+    (9, 4, 5), 
+    (10, 4, 6), 
+    (11, 5, 6), 
+    (12, 6, 6), 
+    (13, 4, 7), 
+    (14, 5, 7), 
+    (15, 6, 7), 
+    (16, 4, 8);
--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,15 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books WHERE books.series_id = 1 ORDER BY books.year ASC" 
 end
 
+# SELECT books.title, books.year FROM books INNER JOIN series ON books.series_id = series.id GROUP BY books.series_id HAVING books.series_id = 1 ORDER BY books.year ASC
+# Why it only returns one item after using GROUP BY & HAVING
+# GROUP BY books.series_id (did not work)
+# GROUP BY series.id HAVING series.id = 1 (did not work)
+# SELECT series.title FROM series INNER JOIN books ON books.series_id = series.id GROUP BY books.series_id HAVING books.series_id = 1 ORDER BY books.year ASC (did not make sense has no year)
+
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
