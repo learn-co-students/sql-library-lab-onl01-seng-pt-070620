62f60ddd923cd25093c282c4e29a9948076a9b4d
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-09-03 13:07:16 -0700

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,13 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
-    (1, 'the_matrix', 1, 1),
-    (2, 'the_ring', 2, 2);
+    (1, 'the matrix', 1, 1),
+    (2, 'the ring', 2, 2);
 
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES 
+INSERT INTO books (id, title, year, series_id) VALUES 
+    (1, 'the power of now', 2010, 1),
+    (2, 'the new earth', 2010, 2),
+    (3, 'sapient', 2010, 3),
+    (4, 'there is a god', 2010, 4),
+    (5, 'enlightenment now', 2010, 5),
+    (6, 'the new jim crow', 2010, 6);
+
+    
