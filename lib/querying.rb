746072e22908b66b8043a8007bfb37db703625e7
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2020-09-06 17:40:36 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,8 +26,8 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) as book_count 
-  FROM character_books 
-  JOIN characters 
+  FROM characters
+  JOIN character_books 
   ON character_books.character_id = characters.id 
   GROUP BY characters.name 
   ORDER BY book_count DESC, characters.name ASC;"
