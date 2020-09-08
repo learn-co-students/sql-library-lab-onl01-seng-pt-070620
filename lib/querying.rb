181922fdd9879944cdd475b28fcd025525edb363
kam3r0nz <kam.hungerford@gmail.com>
2020-09-08 13:11:32 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
-  FROM books JOIN series 
-  ON books.series_id = series.id 
+  FROM books 
+  JOIN series ON books.series_id = series.id 
   WHERE series.id = 1 
   ORDER BY books.year;"
 end
@@ -23,20 +23,16 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name 
   FROM authors 
-  JOIN series 
-  ON series.author_id = authors.id 
-  JOIN subgenres 
-  ON series.subgenre_id = subgenres.id;"
+  JOIN series ON series.author_id = authors.id 
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series JOIN books 
   ON books.series_id = series.id 
-  JOIN character_books 
-  ON character_books.book_id = books.id 
-  JOIN characters 
-  ON character_books.character_id = characters.id 
+  JOIN character_books ON character_books.book_id = books.id 
+  JOIN characters ON character_books.character_id = characters.id 
   WHERE characters.species = 'human' 
   GROUP BY series.title 
   ORDER BY COUNT(*) DESC LIMIT 1;"
@@ -46,8 +42,7 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) 
   AS num_books 
   FROM character_books
-  INNER JOIN characters
-  ON character_books.character_id = characters.id
+  INNER JOIN characters ON character_books.character_id = characters.id
   GROUP BY characters.name
   ORDER BY num_books DESC;"
 end
