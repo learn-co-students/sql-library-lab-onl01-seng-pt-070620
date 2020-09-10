41e1046215cde518686c5d06512e4c7acacda309
github username <github email address>
2020-09-10 16:24:08 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -50,11 +50,5 @@ def select_character_names_and_number_of_books_they_are_in
   GROUP BY characters.name
   ORDER BY COUNT(name) DESC;"
 
-  # "SELECT characters.name, COUNT(*) as book_count from character_books 
-  # JOIN characters ON character_books.character_id = characters.id 
-  # GROUP BY characters.name ORDER BY book_count DESC"
-
-  # "SELECT characters.name, COUNT(*) as book_count from character_books 
-  # JOIN characters ON character_books.character_id = characters.id 
-  # GROUP BY characters.name ORDER BY book_count DESC"
+ 
 end
