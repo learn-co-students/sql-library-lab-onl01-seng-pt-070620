e9a22f9dc4c154a6d6a575d6cba072b6a884692d
Srog1989 <s.rogers0715@yahoo.com>
2020-09-24 20:28:31 -0600

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE series (id INTEGER PRIMARY KEY, title, author_id, subgenre_id);
+
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name);
+
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name);
+
+CREATE TABLE books (id INTEGER PRIMARY KEY, title, year, series_id);
+
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name, species, motto, author_id);
+
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id, character_id);
